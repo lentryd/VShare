8ceabe8a809d2704d9ec3de95a1a5ba57e80d714
lentryd <lentryd@users.noreply.github.com>
2022-09-29 16:46:40 +0000

    update content

---
+++ precache-manifest.78c3fa294f1d947c7aae91052fc232e7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f94160aac8a40c9fcc0",
+    "url": "/VShare/css/app.01321a59.css"
+  },
+  {
+    "revision": "92b7a8e5fa348227d7c8",
+    "url": "/VShare/css/chunk-0dfefd9e.14f87756.css"
+  },
+  {
+    "revision": "977943b9567cfe8628d5",
+    "url": "/VShare/css/chunk-249d13de.7e2f84ba.css"
+  },
+  {
+    "revision": "a50eb00b5f26e06f8bf9",
+    "url": "/VShare/css/chunk-4c2b1572.3e565104.css"
+  },
+  {
+    "revision": "b85697e3b26c929f491bcf11537e9a98",
+    "url": "/VShare/img/0.b85697e3.png"
+  },
+  {
+    "revision": "dee3234930844a941e230cff13194dbf",
+    "url": "/VShare/img/1.dee32349.png"
+  },
+  {
+    "revision": "718c12a228039d716ca301d49c1e6da9",
+    "url": "/VShare/img/10.718c12a2.png"
+  },
+  {
+    "revision": "efa98e12c92dae75a1102287119fa7c8",
+    "url": "/VShare/img/11.efa98e12.png"
+  },
+  {
+    "revision": "cd33f7ca1988369f67773fa125a77a94",
+    "url": "/VShare/img/12.cd33f7ca.png"
+  },
+  {
+    "revision": "bc23b2eb119ce14f5fda5ca396c9e4ce",
+    "url": "/VShare/img/13.bc23b2eb.png"
+  },
+  {
+    "revision": "f0a4107263d0518dabb7e90b410920e3",
+    "url": "/VShare/img/2.f0a41072.png"
+  },
+  {
+    "revision": "75c560a6e2a6ace2593cad7738367145",
+    "url": "/VShare/img/3.75c560a6.png"
+  },
+  {
+    "revision": "6bc5fc213bd9ddcb61679b11a5304932",
+    "url": "/VShare/img/4.6bc5fc21.png"
+  },
+  {
+    "revision": "9babf4e47db77de27f65b537ece6e0e6",
+    "url": "/VShare/img/5.9babf4e4.png"
+  },
+  {
+    "revision": "a7684579293083bd618f77ac56cd0076",
+    "url": "/VShare/img/6.a7684579.png"
+  },
+  {
+    "revision": "96755dfffbfa20009b62eb17c573b450",
+    "url": "/VShare/img/7.96755dff.png"
+  },
+  {
+    "revision": "7db98765919fee79572c0d968943584a",
+    "url": "/VShare/img/8.7db98765.png"
+  },
+  {
+    "revision": "63c215203e13dabcd5617528eb157e8d",
+    "url": "/VShare/img/9.63c21520.png"
+  },
+  {
+    "revision": "ee6ff496de9ff2d853d631db155bdb56",
+    "url": "/VShare/img/android-chrome-192x192.ee6ff496.png"
+  },
+  {
+    "revision": "97ea264b135982df2a26327013ceca7b",
+    "url": "/VShare/img/android-chrome-512x512.97ea264b.png"
+  },
+  {
+    "revision": "5a772ab42c8015ab96929ae2364b82e4",
+    "url": "/VShare/img/apple-touch-icon.5a772ab4.png"
+  },
+  {
+    "revision": "89edd7136120333660bbba8c262a8b51",
+    "url": "/VShare/index.html"
+  },
+  {
+    "revision": "9f94160aac8a40c9fcc0",
+    "url": "/VShare/js/app.a91117e0.js"
+  },
+  {
+    "revision": "92b7a8e5fa348227d7c8",
+    "url": "/VShare/js/chunk-0dfefd9e.3763167b.js"
+  },
+  {
+    "revision": "977943b9567cfe8628d5",
+    "url": "/VShare/js/chunk-249d13de.13923625.js"
+  },
+  {
+    "revision": "a50eb00b5f26e06f8bf9",
+    "url": "/VShare/js/chunk-4c2b1572.d4f129b8.js"
+  },
+  {
+    "revision": "eef32efc6c1f52aa4ffe",
+    "url": "/VShare/js/chunk-vendors.5b76c4b5.js"
+  },
+  {
+    "revision": "41d1a907011f04d697a7cc409e40825f",
+    "url": "/VShare/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VShare/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/VShare/precache-manifest.78c3fa294f1d947c7aae91052fc232e7.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vdocs"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"));
+
+workbox.routing.registerRoute(/^http.*github/, new workbox.strategies.NetworkFirst({ "cacheName":"api-cache","networkTimeoutSeconds":2, plugins: [] }), 'GET');
